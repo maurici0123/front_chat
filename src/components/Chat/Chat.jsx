f313e4377cfe7a59aa67551c2201d8c0c1f11201
mauricio <mauriciosilvajunior12@gmail.com>
2024-09-07 12:46:26 -0300

    upgrade

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,19 +1,74 @@
+import './ChatStyle.css'
 import React, { useRef, useState, useEffect } from 'react'
+import { VscSend } from "react-icons/vsc"
 
 export default function Chat(props) {
 
+    const userId = localStorage.getItem('userId')
+
+    const bottomRef = useRef()
     const messageRef = useRef()
     const [messageList, setMessageList] = useState([])
+    const [heightSendInput, setHeightSendInput] = useState(40)
 
     useEffect(() => {
+        localStorage.getItem('messages') && setMessageList(JSON.parse(localStorage.getItem('messages')))
+    }, [])
+
+    useEffect(() => {
+
         props.socket.on('recive_message', data => {
-            setMessageList((current) => [...current, data])
+
+
+            setMessageList(current => {
+                const upadateMessage = [...current, data]
+                localStorage.setItem('messages', JSON.stringify(upadateMessage))
+                //localStorage.clear()
+                return upadateMessage
+            })
         })
 
         return () => {
             props.socket.off('recive_message')
         }
     }, [props.socket])
+
+    useEffect(() => {
+        scrollDown()
+    }, [messageList])
+
+    const clearInput = () => messageRef.current.value = ''
+
+    const focusInput = () => messageRef.current.focus()
+
+    const scrollDown = () => bottomRef.current.scrollIntoView()
+
+    const getEnterKey = (e) => {
+        if (e.code === 'Enter') {
+            if (e.shiftKey || e.ctrlKey) {
+                const textarea = messageRef.current
+                const start = textarea.selectionStart
+                const end = textarea.selectionEnd
+                textarea.value = `${textarea.value.substring(0, start)}\n${textarea.value.substring(end)}`
+                textarea.selectionStart = textarea.selectionEnd = start + 1
+
+                const lineCount = textarea.value.split('\n').length
+                if (lineCount <= 5) {
+                    setHeightSendInput(prev => prev += 24)
+                }
+            } else {
+                handleSubmit()
+            }
+            e.preventDefault()
+        }
+    }
+    
+    const isLastTwoMessagesSameAuthor = (index) => {
+        if (index > 0) {
+            return messageList[index].authorId === messageList[index - 1].authorId
+        }
+        return false
+    }
 
     const handleSubmit = () => {
         const message = messageRef.current.value
@@ -24,30 +79,41 @@ export default function Chat(props) {
         focusInput()
     }
 
-    const clearInput = () => {
-        messageRef.current.value = ''
-    }
-
-    const focusInput = () => {
-        messageRef.current.focus()
-    }
-
-    const getEnterKey = (e) => {
-        if (e.code === 'Enter') {
-            handleSubmit()
-        }
-    }
-
     return (
-        <div>
-            <h1>chat</h1>
-            {
-                messageList.map((message, index) => (
-                    <p key={index} >{message.author}: {message.text}</p>
-                ))
-            }
-            <input type="text" ref={messageRef} onKeyDown={e => getEnterKey(e)} placeholder='Mensagem' />
-            <button onClick={() => handleSubmit()}>Enviar</button>
+        <div className='chat'>
+            <div className='chat-area'>
+                <div className='conversation'>
+                    {
+                        messageList.map((message, index) => (
+                            <div key={index} className={`message-area 
+                                ${message.authorId === userId ? 'my-message-area' : 'other-message-area'}
+                                ${isLastTwoMessagesSameAuthor(index) && 'pasted-message'}`}>
+
+                                {/* {console.log(message.authorId)} */}
+                                {/* {console.log(userId)} */}
+
+                                <div className={`message ${message.authorId === userId ? 'my-message' : 'other-message'}`}>
+                                    <p
+                                        className={`author ${message.authorId === userId ? 'my-author' : 'other-author'}
+                                        ${isLastTwoMessagesSameAuthor(index) && 'author-pasted'}`}>{message.author}:
+                                    </p>
+
+                                    <p>{message.text}</p>
+
+                                    <p className='time'>{message.time}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                    <div ref={bottomRef} />
+                </div>
+
+                <div className='input-area'>
+                    <textarea type="text" style={{ height: `${heightSendInput}px` }}
+                        className='send-input' ref={messageRef} onKeyDown={e => getEnterKey(e)} placeholder='Mensagem' />
+                    <button className='send-button' onClick={() => handleSubmit()}><VscSend className='send-icon' /></button>
+                </div>
+            </div>
         </div>
     )
 }
